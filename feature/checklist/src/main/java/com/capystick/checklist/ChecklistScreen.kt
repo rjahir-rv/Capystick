ee39c9d2b49aefe1415f28d60404499207d57b9b
rjahir-rv <rjahir.rv@gmail.com>
2026-05-14 18:26:14 -0600

    Refactor checklist feature into modular components and apply UI refinements to note cards, navigation, and settings

--- feature/checklist/src/main/java/com/capystick/checklist/ChecklistScreen.kt
+++ feature/checklist/src/main/java/com/capystick/checklist/ChecklistScreen.kt
@@ -17,10 +17,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLayoutDirection
