184ed97f4804d3a1d0677d606afca96ad9c1158e
rjahir-rv <rjahir.rv@gmail.com>
2026-05-28 13:23:52 -0600

    Remove advertising ID permission and update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.capystick.app"
         minSdk = 28
         targetSdk = 37
-        versionCode = 12
+        versionCode = 13
         versionName = "0.9.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
